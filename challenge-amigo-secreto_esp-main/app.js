61069fb3d70bb3210b065e118a573f3d073e5a78
Matias <canibanomatias@gmail.com>
2025-03-18 16:15:28 -0300

    Inicializando repositorio

---
+++ challenge-amigo-secreto_esp-main/app.js
@@ -0,0 +1,23 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+const inputAmigo = document.getElementById ("amigo");
+const listaAmigos = [];
+const ulListaAmigos = document.getElementById ("listaAmigos");
+const ulResultado = document.getElementById ("resultado")
+
+if(inputAmigo.value ==""){
+    alert("Debes ingresar un nombre")
+}
+function agregarAmigo(){
+    listaAmigos.push(inputAmigo.value);  
+    ulListaAmigos.innerHTML += `<li>${inputAmigo.value}</li>`;
+};
+
+
+function sortearAmigo(){
+    const random = Math.floor(Math.random() * listaAmigos.length);
+    const amigoSecreto = listaAmigos[random];
+    ulResultado.innerHTML = `<li>El amigo secreto es: ${amigoSecreto}</li>`;
+
+}
